cddc4db47241b1fe14d17b6e57473048b109da3b
Kanaan Hurst <kjhurst05@gmail.com>
2026-09-07 11:43:15 -0500

    new_commit

--- project_sql/6_count_of_top_paying_skills.sql
+++ project_sql/6_count_of_top_paying_skills.sql
@@ -18,11 +18,13 @@ WITH top_paying_jobs AS (
 )
 
 SELECT
-    top_paying_jobs.*,
-    skills
+    skills,
+    COUNT(skills) AS count_of_skills
 FROM
     top_paying_jobs
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-
-
+GROUP BY 
+    skills
+ORDER BY 
+    COUNT(skills) DESC
